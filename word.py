33a7f68782f5f18fe9fae02fc4d1afbf3f815c62
Avishith <pmshithu143@gmail.com>
2020-09-01 15:17:24 +0530

    list added for the game and connection astabilshed for check button

--- word.py
+++ word.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import random
 global list 
 
 
@@ -7,10 +8,13 @@ global list
 class Test():
 	def __init__(self):
 		self.root = tk.Tk()
-		self.text = tk.StringVar()
-		self.text.set("Test")
-		list=["hai","hello"]
+		self.text= tk.StringVar()
+		self.ans=tk.StringVar()
 		
+		
+		self.list=("FO_R",'BO_K','MOB_LE')
+		self.qus=self.list[random.randint(0,2)]
+		self.text.set(self.qus)
 		self.ABC=tk.Frame(self.root,bg='#000000',bd=20,)
 		self.ABC.pack()
 		
@@ -21,12 +25,17 @@ class Test():
 		
 		self.label=tk.Label(self.ABC,bg='black',textvariable=self.text,fg='green',font=("arial",13,'bold')).pack()
 		
-		self.btn=tk.Entry(self.ABC,).pack()
+		self.Entry=tk.Entry(self.ABC,textvariable=self.ans).pack()
 		
 		self.btn3=tk.Button(self.ABC,text='check',command=self.change).pack()
 		self.root.mainloop()
 	def change(self):
-		self.text.set("hai")
+		a=self.ans.get()
+		a=a.upper()
+		if a in self.list:
+			self.text.set('wow')
+		else :
+			self.text.set('bad move')
 		
 		
 app=Test()
